d8b68ec797411026c8f6afc5f35d7fbcd663f87b
gvozdikaaa <ameridiya@gmail.com>
2026-06-17 23:27:00 +0300

    Update mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -73,10 +73,6 @@ async function sendResultsToPsychologist(user, reportText, recipientEmail) {
 }
 
 async function verifyConnection() {
-    console.log('SMTP_HOST =', process.env.SMTP_HOST);
-console.log('SMTP_PORT =', process.env.SMTP_PORT);
-console.log('SMTP_USER =', process.env.SMTP_USER);
-console.log('SMTP_PASS exists =', !!process.env.SMTP_PASS);
     const t = getTransporter();
     if (!t) {
         console.warn('[mailer] SMTP не настроен: проверьте SMTP_USER и SMTP_PASS в .env');
